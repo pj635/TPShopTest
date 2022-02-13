22135b62e7f22949e97fc68bad9e18a5e991ab45
pj635 <2559394890@qq.com>
2022-02-13 21:20:15 +0800

    删除多余注释内容

--- page/loginPage.py
+++ page/loginPage.py
@@ -1,5 +1,4 @@
 from selenium.webdriver.common.by import By
-
 from page.basePage import BasePage
 
 
--- page/mainPage.py
+++ page/mainPage.py
@@ -1,5 +1,4 @@
 from selenium.webdriver.common.by import By
-
 from page.basePage import BasePage
 from page.loginPage import LoginPage
 
@@ -8,20 +7,7 @@ class MainPage(BasePage):
     def __init__(self, driver = None):
         super().__init__(driver)
 
-    # def goto_calendar(self):
-    #     self.driver.find_element_by_xpath('//*[@class="_pp-product-container"]').click()
-    #     self.driver.find_element_by_xpath('//*[@title="日历"]').click()
-    #     self.driver.switch_to.window(self.driver.window_handles[-1])
-    #     return CalendarPage(self.driver)
-
     # 进入登录页面
     def goto_login(self):
         self.driver.find_element(By.XPATH, '//*[text()="登录"]').click()
         return LoginPage(self.driver)
-
-    # # 验证是否已经登录
-    # def is_login(self):
-    #     ele = self.driver.find_element('//*[text()="安全退出"]')
-    #     if ele:
-    #         return True
-    #     return False
