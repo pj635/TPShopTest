e08355fb8cf4c1fcf5e4b9715c886068d58c1219
pj635 <2559394890@qq.com>
2022-02-13 21:13:45 +0800

    修改用例名称

--- testcase/test_login.py
+++ testcase/test_login.py
@@ -1,5 +1,3 @@
-import time
-
 import pytest
 import yaml
 
@@ -18,12 +16,12 @@ class TestLogin:
     # 登录成功的测试用例
     @pytest.mark.parametrize('login_dic', yaml.safe_load(open("./login.yaml", "r", encoding='utf-8'))['login_success'])
     def test_login_success(self, login_dic):
+
         res = self.loginPage.login_success(login_dic['username'], login_dic['passwd'], login_dic['verifycode'])
         assert res == True
-        time.sleep(5)
 
     # 登录失败的测试用例
     @pytest.mark.parametrize('login_dic', yaml.safe_load(open("./login.yaml", "r", encoding='utf-8'))['login_fail'])
-    def test_login_success(self, login_dic):
+    def test_login_fail(self, login_dic):
         res = self.loginPage.login_fail(login_dic['username'], login_dic['passwd'], login_dic['verifycode'])
         assert res == login_dic['result']
